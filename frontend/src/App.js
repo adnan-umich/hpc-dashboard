6ea8485dfbfcb277ec85f7fc8c2efdfd41aadc1d
adnanzai <adnanzai@umich.edu>
2024-05-24 21:29:58 -0400

    no dark/light toggle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useMemo, useContext } from 'react';
-import { Container, createTheme, ThemeProvider, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, colors } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import InputBase from '@mui/material/InputBase';
@@ -16,12 +16,10 @@ import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import Stack from '@mui/material/Stack';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
 import GL from './GL';
 import LH from './LH';
 import A2 from './A2';
-import './App.css';
+import './App.css'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -64,55 +62,13 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-const ToggleColorMode = ({ children }) => {
-  const [mode, setMode] = useState('light');
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-};
-
-const MyApp = () => {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
-  return (
-    <Box>
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
-  );
-};
-
 const App = () => {
   const [selectedComponent, setSelectedComponent] = useState('Great Lakes');
   const [searchValue, setSearchValue] = useState('');
   const [submittedSearch, setSubmittedSearch] = useState('');
-  const [time, setTime] = useState('10');
-  const [open, setOpen] = useState(false);
+
+  const [time, setTime] = React.useState('10');
+  const [open, setOpen] = React.useState(false);
 
   const handleChange = (event) => {
     setTime(event.target.value);
@@ -132,9 +88,9 @@ const App = () => {
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
-    setSubmittedSearch(searchValue.trim());
+    setSubmittedSearch(searchValue.trim()); // Remove leading/trailing whitespace
   };
-
+  
   const renderComponent = () => {
     switch (selectedComponent) {
       case 'Great Lakes':
@@ -149,95 +105,92 @@ const App = () => {
   };
 
   return (
-    <ToggleColorMode>
-      <Container>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar sx={{ backgroundColor: '#00274C' }}>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-              >
-                <PopupState variant="popover" popupId="demo-popup-menu">
-                  {(popupState) => (
-                    <React.Fragment>
-                      <MenuIcon variant="contained" {...bindTrigger(popupState)} />
-                      <Menu {...bindMenu(popupState)}>
-                        <MenuItem
-                          onClick={() => {
-                            setSelectedComponent('Great Lakes');
-                            popupState.close();
-                          }}
-                        >
-                          Great Lakes
-                        </MenuItem>
-                        <MenuItem
-                          onClick={() => {
-                            setSelectedComponent('Armis2');
-                            popupState.close();
-                          }}
-                        >
-                          Armis2
-                        </MenuItem>
-                        <MenuItem
-                          onClick={() => {
-                            setSelectedComponent('Lighthouse');
-                            popupState.close();
-                          }}
-                        >
-                          Lighthouse
-                        </MenuItem>
-                      </Menu>
-                    </React.Fragment>
-                  )}
-                </PopupState>
-              </IconButton>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-              >
-                {selectedComponent} Dashboard
-              </Typography>
-              <Stack direction={{ xs: 'column', md: 'row' }}>
-                <TimerIcon sx={{ color: '#2F65A7', padding: '0.6em 0em 0em 0em' }} />
-                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                  <Select
-                    value={time}
-                    onChange={handleChange}
-                    sx={{ color: 'white', borderColor: 'white' }}
-                    defaultValue={10}
-                  >
-                    <MenuItem value={10}>Today</MenuItem>
-                    <MenuItem value={20}>Last 6 Months</MenuItem>
-                    <MenuItem value={30}>This Year</MenuItem>
-                  </Select>
-                </FormControl>
-              </Stack>
-              <form onSubmit={handleSearchSubmit}>
-                <Search>
-                  <SearchIconWrapper>
-                    <SearchIcon />
-                  </SearchIconWrapper>
-                  <StyledInputBase
-                    placeholder="Slurm account..."
-                    inputProps={{ 'aria-label': 'search' }}
-                    value={searchValue}
-                    onChange={handleSearchChange}
-                  />
-                </Search>
-              </form>
-              <MyApp />
-            </Toolbar>
-          </AppBar>
-        </Box>
-        {renderComponent()}
-      </Container>
-    </ToggleColorMode>
+    <Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ backgroundColor: '#00274C' }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <React.Fragment>
+                    <MenuIcon variant="contained" {...bindTrigger(popupState)} />
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem
+                        onClick={() => {
+                          setSelectedComponent('Great Lakes');
+                          popupState.close();
+                        }}
+                      >
+                        Great Lakes
+                      </MenuItem>
+                      <MenuItem
+                        onClick={() => {
+                          setSelectedComponent('Armis2');
+                          popupState.close();
+                        }}
+                      >
+                        Armis2
+                      </MenuItem>
+                      <MenuItem
+                        onClick={() => {
+                          setSelectedComponent('Lighthouse');
+                          popupState.close();
+                        }}
+                      >
+                        Lighthouse
+                      </MenuItem>
+                    </Menu>
+                  </React.Fragment>
+                )}
+              </PopupState>
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              {selectedComponent} Dashboard
+            </Typography>
+            <Stack direction={{ xs: 'column', md: 'row'}}>
+            <TimerIcon sx ={{color: '#2F65A7', padding: '0.6em 0em 0em 0em' }} />
+              <FormControl sx={{ m: 1, minWidth: 120}} size="small">
+                <Select
+                  value={time}
+                  onChange={handleChange}
+                  sx ={{ color: 'white', borderColor: 'white'}}
+                  defaultValue={10}
+                > 
+                  <MenuItem value={10}>Today</MenuItem>
+                  <MenuItem value={20}>Last 6 Months</MenuItem>
+                  <MenuItem value={30}>This Year</MenuItem>
+                </Select>
+              </FormControl>
+            </Stack>
+            <form onSubmit={handleSearchSubmit}>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Slurm account..."
+                  inputProps={{ 'aria-label': 'search' }}
+                  value={searchValue}
+                  onChange={handleSearchChange}
+                />
+              </Search>
+            </form>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {renderComponent()}
+    </Container>
   );
 };
 
